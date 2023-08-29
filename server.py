60288ff0dc4a67b252723c9bfb990bb60d9bef9e
PathyFernandez <116598443+Pathyfernandez@users.noreply.github.com>
2023-08-28 22:15:23 -0400

    Python_ninja

---
+++ server.py
@@ -0,0 +1,6 @@
+from app_derby_py_pie import app
+from app_derby_py_pie.controladores import controlador_usuario
+from app_derby_py_pie.controladores import controlador_pie
+
+if __name__ =='__main__':
+    app.run(debug=True, port= 5000)
